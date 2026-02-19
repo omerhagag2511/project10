dcf569eed4ea5f6ff375a8ff6edd79d205dbc0cc
omerhagag2511 <omerhagag2511@gmail.com>
2026-02-19 17:36:20 +0200

    3rd commit

--- app/src/main/java/com/example/project10/MainActivity.java
+++ app/src/main/java/com/example/project10/MainActivity.java
@@ -8,37 +8,125 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText et1;
-    int num = 0;
+    double num;
+    double num2;
     int sign = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        et1 = (EditText) findViewById(R.id.text1);
     }
     public void minus(View view) {
         num = Integer.parseInt(et1.getText().toString());
         if (sign == 1){
-
+            num2 = num2 - num;
         }
         else if(sign == 2){
-
+            num2 = num2 +num;
         }
-        else if (sign == 3){
-
+        else if (sign == 3) {
+            num2 = num2 * num;
+        }
+        else if (sign == 4) {
+            num2 = num2 / num;
+        }
+        else{
+            num2 = num;
         }
         sign = 1;
+        et1.setText("");
+        et1.setHint(String.valueOf(num2));
     }
     public void plus(View view) {
         num = Integer.parseInt(et1.getText().toString());
+        if (sign == 1){
+            num2 = num2 - num;
+        }
+        else if(sign == 2){
+            num2 = num2 +num;
+        }
+        else if (sign == 3) {
+            num2 = num2 * num;
+        }
+        else if (sign == 4) {
+            num2 = num2 / num;
+        }
+        else{
+            num2 = num;
+        }
+        sign = 2;
+        et1.setText("");
+        et1.setHint(String.valueOf(num2));
     }
     public void times(View view) {
         num = Integer.parseInt(et1.getText().toString());
+        if (sign == 1){
+            num2 = num2 - num;
+        }
+        else if(sign == 2){
+            num2 = num2 +num;
+        }
+        else if (sign == 3) {
+            num2 = num2 * num;
+        }
+        else if (sign == 4) {
+            num2 = num2 / num;
+        }
+        else{
+            num2 = num;
+        }
+        sign = 3;
+        et1.setText("");
+        et1.setHint(String.valueOf(num2));
+    }
+    public void div(View view) {
+        num = Integer.parseInt(et1.getText().toString());
+        if (sign == 1){
+            num2 = num2 - num;
+        }
+        else if(sign == 2){
+            num2 = num2 +num;
+        }
+        else if (sign == 3) {
+            num2 = num2 * num;
+        }
+        else if (sign == 4) {
+            num2 = num2 / num;
+        }
+        else{
+            num2 = num;
+        }
+        sign = 4;
+        et1.setText("");
+        et1.setHint(String.valueOf(num2));
     }
     public void equal(View view) {
         num = Integer.parseInt(et1.getText().toString());
+        if (sign == 1){
+            num2 = num2 - num;
+        }
+        else if(sign == 2){
+            num2 = num2 +num;
+        }
+        else if (sign == 3) {
+            num2 = num2 * num;
+        }
+        else if (sign == 4) {
+            num2 = num2 / num;
+        }
+        else{
+            num2 = num;
+        }
+        et1.setHint(String.valueOf(num2));
+        et1.setText("");
+        sign = 0;
     }
+
     public void clear(View view) {
-        num = Integer.parseInt(et1.getText().toString());
+        et1.setText("");
+        sign = 0;
+        num = 0;
+        num2= 0;
     }
 }
